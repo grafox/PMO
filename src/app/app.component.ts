1a01364523a7d3b9969a604978d1d9515cdc1c6f
grafox <g_fox_fts@yahoo.com>
2018-03-10 19:56:40 +0300

    DatePipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
 //import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
 import { AngularFirestore } from 'angularfire2/firestore';
 import { Observable } from 'rxjs/Observable';
@@ -9,19 +10,13 @@ import { Observable } from 'rxjs/Observable';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'مجلس الوزراء يعقد جلسته الاعتيادية برئاسة رئيس مجلس الوزراء الدكتور حيدر العبادي';
-  subContent ='عقد مجلس الوزراء جلسته الاعتيادية اليوم الثلاثاء 6 آذار 2018 برئاسة رئيس مجلس الوزراء الدكتور حيدر العبادي. ووجّه مجلس الوزراء شكره وتقديره لمبادرة عاهل المملكة العربية السعودية الملك سلمان بن عبد العزيز بإنشاء ملعب رياضي متكامل في العاصمة بغداد، واوعز المجلس بتشكيل لجنة من(وزارة الشباب والرياضة والامانة العامة لمجلس الوزراء ووزارة التخطيط / دائرة التنمية المحلية والمكانية ووزارة التعليم العالي والبحث العالي/ مركز التخطيط الحضري والاقليمي) تتولى صياغة الاطار العام لإنشاء الملعب بالتنسيق مع المجلس التنسيقي العراقي- السعودي من حيث الموقع والتصميم والمتابعة، وتعرض اللجنة نتائج عملها على مجلس الوزراء';
-  by = 'المكتب الإعلامي لرئيس الوزراء';
+ 
   readMore = 'أقرأ المزيد';
   searchPlaceholder = 'بحث';
   PMO='المكتب الاعلامي لرئاسة الوزراء';
-  from ='من';
-  type = 'بيان'
-  creatAt = '6/3/2018'
-  image = 'https://firebasestorage.googleapis.com/v0/b/firebase-egav.appspot.com/o/images%2F4-3-201802.jpg?alt=media&token=8ed21494-6c7f-4775-8fc8-a9cf4520f987';
-
-  items: Observable<any[]>;
+  in ='في'
+  postesArabic: Observable<any[]>;
   constructor(db: AngularFirestore) {
-    this.items = db.collection('items').valueChanges();
+    this.postesArabic = db.collection('postesArabic').valueChanges();
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+
 import { NgModule,  NO_ERRORS_SCHEMA } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
