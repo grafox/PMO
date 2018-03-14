bf75436d28c819074b73cbc4f92a99724ac5a21b
grafox <g_fox_fts@yahoo.com>
2018-03-14 13:09:19 +0300

    ok

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -32,15 +32,15 @@ export class DetailsComponent implements OnInit {
     private afs: AngularFirestore,
     private route: ActivatedRoute,
     private router: Router) {
-      
-    }
-    ngOnInit() {
-      this.route.params.subscribe((params: Params) => {
-        console.log(params);
-        this.postId = params;
-      })
-      this.postDoc = this.afs.doc('postesArabic/' + this.postId);
-    this.data = this.postDoc.snapshotChanges().map(Action =>{
+
+  }
+  ngOnInit() {
+    this.route.params.subscribe((params: Params) => {
+      console.log(params);
+      this.postId = params;
+    })
+    this.postDoc = this.afs.doc('postesArabic/' + this.postId);
+    this.data = this.postDoc.snapshotChanges().map(Action => {
       const data = Action.payload.data() as Post;
       const id = Action.payload.id;
       return { id, data };
